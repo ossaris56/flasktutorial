831b04830d0a3ab9c4334b1c5a1681d9e0f8395c
ossaris56 <ossaris56@gmail.com>
2019-06-25 01:31:20 +0800

    some progress

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, redirect
 
 app = Flask(__name__)
 
@@ -6,8 +6,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/post")
+
+@app.route('/postdiscussion', methods=['POST', 'GET'])
+def post_page():
+   return render_template('post.html')
+
 def post():
-    return render_template('post.html')
+    if request.method == 'POST':
+        with open('post.json', 'w') as f:
+            json.dump(request.form, f)
+            return redirect('/')
+        return redirect('/post_page')
 
 app.run(debug=True)
