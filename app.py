70bf65f6e087810b77977dfb56f842d438e63fca
ossaris56 <ossaris56@gmail.com>
2019-06-26 02:21:10 +0800

    added unique comment pages

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+
+with open('posts.json', "r") as f:
+    posts = json.load(f)
+
 @app.route("/")
 def index():
-    # reading the posts
-    with open('posts.json', "r") as f:
-        posts = json.load(f)
     # getting time difference of posts
     now = datetime.now()
     time = []
@@ -30,15 +31,22 @@ def post():
     formdata['date'] = datetime.now()
     formdata['comments'] = []
     if len(posts) < 1:
-        formdata['id'] = 1
+        formdata['post_id'] = 1
     else:
-        formdata['id'] = posts[-1]['id'] + 1
+        formdata['post_id'] = posts[-1]['post_id'] + 1
     posts.append(formdata)
+
     # write the post
     with open('posts.json', "w") as f:
         json.dump(posts, f, indent=2, separators=(',', ':'))
 
     return redirect('/')
 
+@app.route('/comments/<int:post_id>')
+def comments(post_id):
+    for post in posts:
+        if post_id == post['post_id']:
+            post_dict = post
+    return render_template('comments.html', post_id=post_id, post_dict=post_dict)
 
 app.run(debug=True)
