5bfb1b720947ff575bff47976a63378a9fe9bdf5
ossaris56 <ossaris56@gmail.com>
2019-06-25 21:45:18 +0800

    correct json woo

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
-from flask import Flask, render_template, request, jsonify, redirect
+from flask import Flask, render_template, request, jsonify, redirect, json
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    # reading the posts
+    with open('posts.json', "r") as f:
+        posts = json.load(f)
+    print(posts)
     return render_template('index.html')
-
 
 @app.route('/postdiscussion', methods=['POST', 'GET'])
 def post_page():
-   return render_template('post.html')
+    return render_template('post.html')
 
+@app.route('/post', methods=['POST', 'GET'])
 def post():
     if request.method == 'POST':
-        with open('post.json', 'w') as f:
-            json.dump(request.form, f)
-            return redirect('/')
-        return redirect('/post_page')
+        # reading the posts
+        with open('posts.json', "r") as f:
+            posts = json.load(f)
+        # add post to posts
+        posts.append(request.form)
+        # write the post
+        with open('posts.json', "w") as f:
+            json.dump(posts, f)
+
+        return redirect('/')
+    else: # GET http method
+        return render_template('post.html')
 
 app.run(debug=True)
